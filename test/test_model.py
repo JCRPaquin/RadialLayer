e772d5179564c821c045672532c0bd1e2e3550c0
Jean-Claude Paquin <jean-claude.paquin@cfa.harvard.edu>
2023-12-08 23:43:12 -0500

    Add unit tests for the base PRL model

---
+++ test/test_model.py
@@ -0,0 +1,31 @@
+import unittest
+
+import torch
+
+from radial_layer.model import PartialRadialLayer
+
+class PartialRadialLayerTests(unittest.TestCase):
+
+    def create_prl(self):
+        return PartialRadialLayer(input_width=16, inner_width=8, depth=3)
+
+    def test_init(self):
+        _ = self.create_prl()
+
+    def test_distribution(self):
+        prl = self.create_prl()
+        dist = prl.distribution_by_angles(torch.linspace(0, 1, 20))
+
+        self.assertEqual(dist.shape, (20, 8))
+        self.assertAlmostEquals((torch.ones(20) - dist.sum(dim=1)).mean().item(), 0)
+
+    def test_output_shape(self):
+        prl = self.create_prl()
+        in_tensor = torch.ones(4, prl.input_width)
+        out_tensor = prl(in_tensor)
+
+        self.assertEqual(out_tensor.shape, (4, prl.inner_width))
+
+
+if __name__ == '__main__':
+    unittest.main()
